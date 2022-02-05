1789671c0eee43ef1b836bdc0aba857b77ba54da
dienoov <alvinodien@yahoo.co.id>
2022-02-05 15:15:19 +0700

    navbar

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -44,9 +45,79 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  Widget navbar() {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/icon_home.svg',
+            color: Colors.grey[600],
+          ),
+          activeIcon: SvgPicture.asset(
+            'assets/icons/icon_home.svg',
+            color: Colors.white,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/icon_message.svg',
+            color: Colors.grey[600],
+          ),
+          activeIcon: SvgPicture.asset(
+            'assets/icons/icon_message.svg',
+            color: Colors.grey[600],
+          ),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/icon_shopping_bag.svg',
+            color: Colors.grey[600],
+          ),
+          activeIcon: SvgPicture.asset(
+            'assets/icons/icon_shopping_bag.svg',
+            color: Colors.grey[600],
+          ),
+          label: 'Cart',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/icon_favorite.svg',
+            color: Colors.grey[600],
+          ),
+          activeIcon: SvgPicture.asset(
+            'assets/icons/icon_favorite.svg',
+            color: Colors.grey[600],
+          ),
+          label: 'Favorite',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/icon_person.svg',
+            color: Colors.grey[600],
+          ),
+          activeIcon: SvgPicture.asset(
+            'assets/icons/icon_person.svg',
+            color: Colors.grey[600],
+          ),
+          label: 'Profile',
+        ),
+      ],
+      backgroundColor: Colors.black,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.grey[600],
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      type: BottomNavigationBarType.fixed,
+      elevation: 0,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: navbar(),
       body: SafeArea(
         child: ListView(
           children: [
